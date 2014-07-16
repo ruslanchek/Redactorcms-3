60200744db73a4cd144e3d50b68f57d6e4b33b2f
Ruslan Shashkov <rshashkov@icloud.com>
2014-07-16 10:35:24 +0400

    яяя

--- Config.class.php
+++ Config.class.php
@@ -1,5 +1,5 @@
 <?php
-//error_reporting(E_ALL | E_STRICT);
+error_reporting(E_ALL ^ E_NOTICE);
 
 session_start();
 date_default_timezone_set('Europe/Moscow');
@@ -14,7 +14,7 @@ Class Config{
     //DB params
     public $db_vars = array(
         'host'  => 'localhost',
-        'db'    => 'redactorcms3',
+        'db'    => 'rdc',
         'user'  => 'root',
         'pass'  => '123'
     );
